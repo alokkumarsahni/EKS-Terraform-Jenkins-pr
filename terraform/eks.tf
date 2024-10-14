25a703ddd8f99c5e4da533229f2268a60aefb693
alokkumarsahni <113391506+alokkumarsahni@users.noreply.github.com>
2024-10-14 21:53:42 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,4 +1,48 @@
-module "eks" {
+provider "aws" {
+  region = "us-east-1"
+}
+#provider
+locals {
+  name   = "my-eks-cluster"
+  region = "us-east-1"
+
+  vpc_cidr = "10.123.0.0/16"
+  azs      = ["us-east-1a", "us-east-1b"]
+
+  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
+  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
+  #intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
+
+  tags = {
+    Example = local.name
+  }
+}
+#VPC
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "~> 4.0"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+										
+
+  enable_nat_gateway = true
+  #enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+
+	}					 
+										 
+   
+ 
+
+	module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.1"
 
@@ -14,23 +58,25 @@ module "eks" {
     }
     vpc-cni = {
       most_recent = true
-    }
+    }		 
   }
+
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.intra_subnets
+  control_plane_subnet_ids = module.vpc.private_subnets
 
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
-    ami_type       = "AL2_x86_64"
-    instance_types = ["m5.large"]
+    ami_type       = "ami-0b0ea68c435eb488d"
+    instance_types = ["t2.medium"]
 
     attach_cluster_primary_security_group = true
-  }
+}
 
-  eks_managed_node_groups = {
-    amc-cluster-wg = {
+
+eks_managed_node_groups = {
+    ascode-cluster-wg = {
       min_size     = 1
       max_size     = 2
       desired_size = 1
@@ -38,9 +84,6 @@ module "eks" {
       instance_types = ["t3.large"]
       capacity_type  = "SPOT"
 
-      tags = {
-        ExtraTag = "helloworld"
-      }
     }
   }
 
